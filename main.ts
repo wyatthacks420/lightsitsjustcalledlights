612a04b49985c59ba9369f3b17a333af7fb54e61
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-04-11 11:04:12 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showIcon(IconNames.Heart)
+wuKong.setLightMode(wuKong.LightMode.BREATH)
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+basic.forever(function () {
+    strip.rotate(1)
+    strip.show()
+})
